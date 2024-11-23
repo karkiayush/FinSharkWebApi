a83783ac85a838cba8fbd4e5248cfe77e80d15ad
karkiayush <karkiayush619@gmail.com>
2024-11-23 15:19:38 +0545

    add stock controller and test it in swagger

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce684e07a581444687ce8be8d1281c590e0f6b53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506126738d50ccec3ebf9b1ce25feb843db25864")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
