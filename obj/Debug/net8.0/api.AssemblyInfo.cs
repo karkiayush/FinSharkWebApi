6a804866284c36e3445a12aa3d71c04bc443fb14
karkiayush <karkiayush619@gmail.com>
2024-11-24 20:09:41 +0545

    Make controller code asynchrounous

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b504cfc72a7c1adf24022edac6905a9d086b48a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d535abe2afab718b238c1fe420337b79c0ed61c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
