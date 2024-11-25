3083aeb2555b2d2cd779f72c12e6c00f716a0021
karkiayush <karkiayush619@gmail.com>
2024-11-25 18:05:56 +0545

    some random changes in file of /obj

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c400adf2f0e805208720a8c0a3c9ab6e01d66116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a155722a48ef001002172ce00e1e0cddf4d50068")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
